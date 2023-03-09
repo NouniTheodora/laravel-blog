d91363df82ae5dae0bfd0effaae935ebc17feae5
Theodora Nouni <nounitheodora@gmail.com>
2023-03-09 19:32:27 +0200

    Route wildcard constraints

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,16 @@
+<title>My Blog</title>
+
+<body>
+    <article>
+        <h1><a href="/articles/article-1">Article 1</a></h1>
+        <p>
+            wrfw
+        </p>
+    </article>
+    <article>
+        <h1><a href="/articles/article-2">Article 2</a></h1>
+        <p>
+            wrfw
+        </p>
+    </article>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/articles', function () {
+    return view('articles');
+});
+
+// Whatever is passed in the url - the same value is passed as a parameter in the function as well
+Route::get('/articles/{article_slug}', function ($article_slug) {
+    $path = __DIR__ . '/../resources/articles/' . $article_slug . '.html';
+
+    if (!file_exists($path)) {
+        // or abort(404);
+        return redirect('/');
+    }
+
+    $article = file_get_contents($path);
+
+    return view('article', [
+        'article' => $article
+    ]);
+})->where('article_slug', '[0-9A-z_\-]+'); // validate the slug - only numbers
