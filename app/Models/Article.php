c719b4baf9004ec34e0b26d1652f71c56302d5c7
Theodora Nouni <nounitheodora@gmail.com>
2023-04-10 20:58:23 +0300

    3 Ways to Mitigate Mass Assignment Vulnerabilities

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    // 1rst way: By using the fillable property we allow mass attribute assignment for this model.
+    // For example we have an update endpoint for the user's information - in that way we ensure
+    // that even if more properties are passed to this endpoint, the only ones that will be updated
+    // will be ONLY the attributes that are included in the $fillable variable.
+    // protected $fillable = [
+    //     'title',
+    //     'excerpt',
+    //     'body'
+    // ];
+
+    // 2nd way: Everything is fillable, except whatever is included in the $guarded.
+    // protected $guarded = [
+    //     'id'
+    // ];
+
+    // 3rd way: Skip it entirely - avoid using arrays that come from forms to functions like create()
+    // or update()
 }
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -2,6 +2,7 @@
     @foreach ($articles as $article)
         <article class="{{ $loop->even ? 'even' : 'odd' }}">
             <a href="articles/{{$article->id}}">{{$article->title}}</a>
+            <div> {{$article->excerpt}} </div>
         </article>
     @endforeach
 </x-layout>
